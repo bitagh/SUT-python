ffb9c26b2586591e7fa801a427c0b63d30d3ad57
bitagh <157855726+bitagh@users.noreply.github.com>
2024-01-27 03:39:22 +0330

    Delete tamrin3 directory

--- tamrin3/name ramz.py
+++
@@ -1,8 +0,0 @@
-def main():
-    items = input().split()
-    items.sort(key=lambda x: int(x[1:]))
-    result = ''.join(item[0] for item in items)
-    print(result)
-
-if __name__ == "__main__":
-    main()
